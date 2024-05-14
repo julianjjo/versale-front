3365f180f99854f3d0099ab005a607bb87548e48
Julian Mican <julian.mican@gmail.com>
2024-05-13 22:06:05 -0500

    Add product by id

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "middleware"
+  })
 });
